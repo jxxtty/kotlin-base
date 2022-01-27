8befdf3e44f800e3f9d9feb7e9cc597efc19641d
jxxtty <jaehee941106@gmail.com>
2022-01-27 11:17:39 +0900

    overloading, default argument, named argument

--- src/main/kotlin/20_overloading_default,named_argument.kt
+++ src/main/kotlin/20_overloading_default,named_argument.kt
@@ -1,7 +1,16 @@
 fun main(){
+    // overloading
     read(7)
     read("7")
 
+    // default argument
+    deliveryItem("책")
+    deliveryItem("짬뽕", 3)
+    deliveryItem("노트북", 30, "회사")
+
+    // named argument
+    // name, destination 값만 전달하고 싶다면?
+    deliveryItem("선물", destination = "칭구집")
 }
 
 fun read(x: Int) {
@@ -10,4 +19,8 @@ fun read(x: Int) {
 
 fun read(x: String){
     println("read-String : $x 입니다")
+}
+
+fun deliveryItem(name: String, count: Int = 1, destination: String = "우리집"){
+    println("$name $count 개를 $destination 에 배달했습니다.")
 }
